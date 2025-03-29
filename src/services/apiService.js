8cf0952b079e35c7ad2c43b04ee176c7854504be
Brandon <brandon.lopez@colombiasoftwrae.net>
2025-03-28 22:53:56 -0500

    AJUSTES

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,5 +1,5 @@
 export async function calculateHours(data) {
-  const response = await fetch('https://falconcloud.co/site_srv10_ph/site/api/qserv.php/13465-770721', {
+  const response = await fetch('https://corsproxy.io/?https://falconcloud.co/site_srv10_ph/site/api/qserv.php/13465-770721', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({
